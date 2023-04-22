6026f2d12ee47800ff0a6500f07e67e4bb4abcfb
CrystalWD <egixx@mail.ru>
2023-04-22 03:50:39 +0300

    MyInitial

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/firsttestbd";
+    private final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final static String DB_USERNAME = "root";
+    private final static String DB_PASSWORD = "ETsvetkov420";
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName(DB_DRIVER);
+            con = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection-OK");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.err.println("Connection Error");
+        }
+        return con;
+    }
+
 }
